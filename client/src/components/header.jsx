1d422ae39ba352ddd540ba593a46f04cabfbf7e6
Derick12345678 <148519593+Derick12345678@users.noreply.github.com>
2025-09-17 13:16:34 -0400

    cv add

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,6 +1,10 @@
 import "../App.css";
 
 export default function Header() {
+  const handleClick = () => {
+    window.open(`client/public/Gallagher_Derek_CV.pdf`, "_blank");
+  };
+
   return (
     <header>
       <nav>
@@ -9,6 +13,7 @@ export default function Header() {
           <li><button type="button" id="aboutme-btn">About Me</button></li>
           <li><button type="button" id="projects-btn">Projects</button></li>
           <li><button type="button" id="contact-btn">Contact</button></li>
+          <li><button type="button" id="cv-btn" onClick={handleClick}>CV</button></li>
         </ul>
       </nav>
     </header>
