56dd4b968fcedf3e9090181addb10ecc28197101
Derick12345678 <148519593+Derick12345678@users.noreply.github.com>
2025-09-20 14:54:21 -0400

    refactoring

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 import './index.css';
-import App from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </StrictMode>
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
-
