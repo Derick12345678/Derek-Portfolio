5edfbc8092255d869df4d9f0c24874a7df46ec6a
Derick12345678 <148519593+Derick12345678@users.noreply.github.com>
2025-09-20 15:17:55 -0400

    refactoring

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -10,7 +10,7 @@ export default function Header() {
           <li><button type="button" id="aboutme-btn">About Me</button></li>
           <li><button type="button" id="projects-btn">Projects</button></li>
           <li><button type="button" id="contact-btn">Contact</button></li>
-          <li><a href="/Gallagher_Derek_CV.pdf" download><button>CV</button></a></li>
+          <li><a href="/Gallagher_Derek_CV.pdf" download="Gallagher_Derek_CV.pdf"><button>CV</button></a></li>
         </ul>
       </nav>
     </header>
