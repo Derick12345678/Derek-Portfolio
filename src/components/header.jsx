606ab9aa80afcc8d03e3542a25044d75d44a2d75
Derick12345678 <148519593+Derick12345678@users.noreply.github.com>
2025-12-16 09:56:29 -0500

    new cv

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -10,7 +10,7 @@ export default function Header() {
           <li><button type="button" id="aboutme-btn">About Me</button></li>
           <li><button type="button" id="projects-btn">Projects</button></li>
           <li><button type="button" id="contact-btn">Contact</button></li>
-          <li><a href="/Derek-Portfolio/Gallagher_Derek_CV.pdf" target="_blank"><button>CV</button></a></li>
+          <li><a href="/Derek-Portfolio/DerekGallagher_CV.pdf" target="_blank"><button>CV</button></a></li>
         </ul>
       </nav>
     </header>
